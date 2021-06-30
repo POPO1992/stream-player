e2b061b63242c86b0b4ff3b124168b9f3f04076a
POPO <popo@cherry.tv>
2021-06-30 23:17:42 +0800

    format with prettier

--- util/browser.ts
+++ util/browser.ts
@@ -1,4 +1,5 @@
-const isNavigatorUndef = () => (typeof navigator === 'undefined')
-export const isIOS = () => !isNavigatorUndef() && /iPad|iPhone|iPod/.test(navigator.platform)
-export const isSafari = () => !isNavigatorUndef() && /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-export const isChrome = () =>!isNavigatorUndef() && /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)
+const isNavigatorUndef = () => typeof navigator === 'undefined';
+export const isIOS = () => !isNavigatorUndef() && /iPad|iPhone|iPod/.test(navigator.platform);
+export const isSafari = () => !isNavigatorUndef() && /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+export const isChrome = () =>
+  !isNavigatorUndef() && /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
